48998ecf789c73a0f5342a1f7e67bbda73feb083
Jokernolove <54339769+Jokernolove@users.noreply.github.com>
2020-01-18 12:34:36 +0800

    Update and rename a.cpp to A - Bone Collector .cpp

--- 一起打牌专题/A - Bone Collector .cpp
+++ 一起打牌专题/A - Bone Collector .cpp
@@ -1,4 +1,37 @@
-//1.0
+//认真对比这几份代码有助于深刻理解DP
+//1.0 dfs暴力搜索（必然会超时，这是为了引出DP）
+#include <iostream>
+#include <string.h>
+using namespace std;
+int value[1010];
+int volume[1010];
+int n,v,t,ans;
+int dfs(int i,int vi)
+{
+    int res;
+    if(i>n) res=0;
+    else if(vi<volume[i]) res=dfs(i+1,vi);
+    else res=max(dfs(i+1,vi),dfs(i+1,vi-volume[i])+value[i]);
+    return res;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    cin>>t;
+    while(t--)
+    {
+	cin>>n>>v;
+	memset(value,0,sizeof(value));
+	memset(volume,0,sizeof(volume));
+	for(int i=1;i<=n;i++) cin>>value[i];
+	for(int i=1;i<=n;i++) cin>>volume[i];
+	ans=dfs(1,v);
+	cout<<ans<<"\n";
+    }
+    return 0;
+}
+//2.0 在dfs暴力搜索的基础引入数组dp储存计算过的值，减少函数调用次数，降低时间复杂度（此时已经能够AC了）
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -33,7 +66,7 @@ int main()
     }
     return 0;
 }
-//2.0
+//2.5  将dfs搜索的递归式改写成有dp数组构成的递推式
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -65,7 +98,7 @@ int main()
     }
     return 0;
 }
-//3.0
+//3.0  将二维数组优化为一维数组，降低空间复杂度
 #include <iostream>
 #include <string.h>
 using namespace std;
