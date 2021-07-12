1b370f3adc6e06384a770cf0f4222b3a0be19248
JokerNoCry <54339769+JokerNoCry@users.noreply.github.com>
2021-07-12 13:49:59 +0800

    Update c.cpp

--- 单调栈&单调队列/c.cpp
+++ 单调栈&单调队列/c.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 2e3+10;
-int dp[maxn][maxn];      //dp[i][j] 第i天持有j股票 赚多少钱
-int t,T,MaxP,W;          //T天数  MaxP最多数量  W间隔天数
-int ap[maxn],bp[maxn];   //ap买入价格  bp卖出价格
-int as[maxn],bs[maxn];   //as买入数量  bs卖出数量
+const int maxn = 2010;
+int t,T,MaxP,W;
+int ap[maxn],bp[maxn],as[maxn],bs[maxn];
+int dp[maxn][maxn];
+int head,tail,que[maxn],num[maxn];
 int main() {
+    
     scanf("%d",&t);
     while(t--) {
         scanf("%d%d%d",&T,&MaxP,&W);
@@ -18,27 +19,41 @@ int main() {
                 dp[i][j] = max(dp[i][j],-j*ap[i]);
             }
         }
-        //如果第i天不交易，那就跟第i-1天一样
-        //如果第i天交易了，那结果取决于i-W-1天
         for(int i=2;i<=T;i++) {
+            dp[i][0] = max(dp[i][0],0);
             for(int j=0;j<=MaxP;j++) {
-                //1.不交易
                 dp[i][j] = max(dp[i][j],dp[i-1][j]);
-                //2.买k股
-                if(i < W+1) continue;
-                for(int k=1;k<=min(j,as[i]);k++) {
-                    dp[i][j] = max(dp[i][j],dp[i-W-1][j-k]-k*ap[i]);
-                }   
-                //3.卖出k股
-                for(int k=1;k<=min(MaxP-j,bs[i]);k++) {
-                    dp[i][j] = max(dp[i][j],dp[i-W-1][j+k]+k*bp[i]);
-                }
+            }
+            //dp[i][j] = max(dp[i][j],dp[i][k] - (j-k)*ap[i]);
+            //  dp[i][k] + k*ap[i] - j*ap[i];
+            //dp[i][j] = max(dp[i][j],dp[i][k] + (k-j)*bp[i]);
+            //  dp[i][k] + k*bp[i] - j*bp[i];
+            if(i < W+1) continue;
+            head = tail = 0;
+            for(int j=0;j<=MaxP;j++) {
+                int temp = dp[i-W-1][j] + j*ap[i];
+                while(head < tail && que[tail-1] < temp) tail--; 
+                que[tail]     = temp;
+                num[tail++] = j;
+                while(head < tail && j-num[head] > as[i]) head++;
+                dp[i][j] = max(dp[i][j],que[head]-j*ap[i]); 
+            }
+            head = tail = 0;
+            for(int j=MaxP;j>=0;j--) {
+                int temp = dp[i-W-1][j] + j*bp[i];
+                while(head < tail && que[tail-1] < temp) tail--;
+                que[tail]     = temp;
+                num[tail++] = j;
+                while(head < tail && num[head]-j > bs[i]) head++;
+                dp[i][j] = max(dp[i][j],que[head]-j*bp[i]);
             }
         }
-        int ans = INT32_MIN;
-        for(int i=0;i<=MaxP;i++) {
+        int ans = 0;
+        for(int i=0;i<=T;i++) {
             ans = max(ans,dp[T][i]);
         }
         printf("%d\n",ans);
     }
+    
+    return 0;
 }
